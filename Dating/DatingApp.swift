ea03423b56415aac06b6c3f4ad2ffff6052a910b
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2025-07-06 18:11:21 +0300

    Initial Commit

---
+++ Dating/DatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingApp.swift
+//  Dating
+//
+//  Created by Антон Разгуляев on 06.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
